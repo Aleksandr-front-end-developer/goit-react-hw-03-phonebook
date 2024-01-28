490ae626630423f9f2125b979bdc95e793642706
Oleksandr Danyliuk <oleksandr.danyliuk.88@gmail.com>
2024-01-28 21:17:49 +0200

    fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -30,7 +30,7 @@ export default class App extends Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    if (prevState.contacts.length !== this.state.contacts) {
+    if (prevState.contacts !== this.state.contacts) {
       localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     }
   }
